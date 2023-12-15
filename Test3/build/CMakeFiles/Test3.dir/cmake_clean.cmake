690db401c82ac85895d445bdf867f55304c119f7
gionchat <gionchat@gmail.com>
2023-12-15 14:28:03 +0800

    init project

---
+++ Test3/build/CMakeFiles/Test3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test3.dir/main.c.o"
+  "CMakeFiles/Test3.dir/testFunc.c.o"
+  "CMakeFiles/Test3.dir/testFunc1.c.o"
+  "Test3"
+  "Test3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Test4/build/CMakeFiles/Test4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test4.dir/main.c.o"
+  "CMakeFiles/Test4.dir/testFunc.c.o"
+  "CMakeFiles/Test4.dir/testFunc1.c.o"
+  "Test4"
+  "Test4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
